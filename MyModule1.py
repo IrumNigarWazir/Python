2d7abaa2caff27744a322260341b41e50fec5f06
IRUM NIGAR WAZIR <irumwazir119@gmail.com>
2025-01-23 13:22:28 +0500

    Add files via upload

---
+++ MyModule1.py
@@ -0,0 +1,14 @@
+import random
+class ganeranl_class:
+    
+    def password(self,n):
+        characters = ['a', 'b', 'c', 'd', 'e', 'f',0,1,2,3,4,5,6,7,8,9]
+        
+        result = random.sample(self.n,characters)
+        return result
+    
+    def sums(self, *numbers):
+        return sum(self.numbers)
+    
+    def typeConvertor(self, number):
+        return str(number) 
